3d5e44c46cc33b8e9085c3128e20d723622b4564
chenhuasheng <chenhuasheng@msn.com>
2021-04-04 02:48:42 +0800

    44444

--- win10tts/Program.cs
+++ win10tts/Program.cs
@@ -11,6 +11,158 @@ using System.Globalization;
 
 namespace win10tts
 {
+    public class MsTTSBuilder : PromptBuilder
+    {
+        public MsTTSBuilder()
+            : base()
+        {
+
+        }
+        public MsTTSBuilder(CultureInfo culture)
+            : base(culture)
+        {
+
+        }
+        public void Add_As_Normal_Text(string textToAdd)
+        {
+            base.AppendText(textToAdd);
+        }
+        public void Add_As_Fast_Text(string textToAdd)
+        {
+            base.AppendText(textToAdd, PromptRate.Fast);
+        }
+        public void Add_As_Slow_Text(string textToAdd)
+        {
+            base.AppendText(textToAdd, PromptRate.Slow);
+        }
+        public void Add_As_SoundLoud_Text(string textToAdd)
+        {
+            base.AppendText(textToAdd, PromptVolume.Loud);
+        }
+        public void Add_As_SoundMedium_Text(string textToAdd)
+        {
+            base.AppendText(textToAdd, PromptVolume.Medium);
+        }
+        public void Add_As_SoundSoft_Text(string textToAdd)
+        {
+            base.AppendText(textToAdd, PromptVolume.Soft);
+        }
+        /// <summary>
+        /// 当作拼读字符串。串里面的英文单词或者数字都会一个一个字母数字读出来，数字不会当作数来读。
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="textToAdd"></param>
+        public void Add_As_SpellOut_Text(string textToAdd)
+        {
+            base.AppendTextWithHint(textToAdd, System.Speech.Synthesis.SayAs.SpellOut);
+        }
+        /// <summary>
+        /// 字符串是当作一个完整的数字来读，而不是拼读。例如：12 读作“十二”，而不是“一二”
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="textToAdd"></param>
+        public void Add_As_Number_Text(string textToAdd)
+        {
+            base.AppendTextWithHint(textToAdd, System.Speech.Synthesis.SayAs.NumberCardinal);
+        }
+        /// <summary>
+        /// 当作电话号码来读，如果前面有区号，还会说出区号来，其余数字会一个一个数字拼读出来。
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="textToAdd"></param>
+        public void Add_As_Telephone_Text(string textToAdd)
+        {
+            base.AppendTextWithHint(textToAdd, System.Speech.Synthesis.SayAs.Telephone);
+        }
+        /// <summary>
+        /// 有日期和时间两部分。请留意返回值。过程先尝试去解释日期时间串，然后再分开加入。这样的串可以是 "2021-12-1 14:23" 也可以是 "12/1/2021 14:23" 或者其他欧美国家熟悉的时间表示法。
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="textToAdd"></param>
+        /// <returns></returns>
+        public bool Add_As_DateTime_Text(string textToAdd)
+        {
+            DateTime dt;
+            if (!DateTime.TryParse(textToAdd, out dt))
+                return false;
+            string sDate = dt.ToString("yyyy-M-d");
+            string sTime = dt.ToString("H:m");
+            base.AppendTextWithHint(sDate, System.Speech.Synthesis.SayAs.YearMonthDay);
+            base.AppendTextWithHint(sTime, System.Speech.Synthesis.SayAs.Time24);
+            return true;
+        }
+        /// <summary>
+        /// 仅有日期部分
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="textToAdd"></param>
+        /// <returns></returns>
+        public bool Add_As_Date_Text(string textToAdd)
+        {
+            DateTime dt;
+            if (!DateTime.TryParse(textToAdd, out dt))
+                return false;
+            base.AppendTextWithHint(dt.ToString("yyyy-M-d"), System.Speech.Synthesis.SayAs.YearMonthDay);
+            return true;
+        }
+        /// <summary>
+        /// 仅有时间部分。例如 1:35pm 或者 13:35
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="textToAdd"></param>
+        public void Add_As_Time_Text(string textToAdd)
+        {
+            base.AppendTextWithHint(textToAdd, System.Speech.Synthesis.SayAs.Time);
+        }
+        /// <summary>
+        /// 12小时制时间表示法。例如 1:35pm
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="textToAdd"></param>
+        public void Add_As_Time12_Text(string textToAdd)
+        {
+            base.AppendTextWithHint(textToAdd, System.Speech.Synthesis.SayAs.Time12);
+        }
+        /// <summary>
+        /// 24小时时间表示法。例如 13:35
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="textToAdd"></param>
+        public void Add_As_Time24_Text(string textToAdd)
+        {
+            base.AppendTextWithHint(textToAdd, System.Speech.Synthesis.SayAs.Time24);
+        }
+
+        public void Add_wav(string filePath)
+        {
+            base.AppendAudio(filePath);
+        }
+        public bool Add_mp3(string filePath)
+        {
+            return true;
+        }
+        public bool Add_wma(string filePath)
+        {
+            return true;
+        }
+        public bool Add_aac(string filePath)
+        {
+            return true;
+        }
+        public void Add_Audio(string audioFilePath)
+        {
+            base.AppendAudio(audioFilePath);
+        }
+        public void Add_Audio(Uri audioFile)
+        {
+            base.AppendAudio(audioFile);
+        }
+        public void Add_Audio(Uri audioFile, string alternateText)
+        {
+            base.AppendAudio(audioFile, alternateText);
+        }
+    }
+
     class Program
     {
         
@@ -224,44 +376,45 @@ namespace win10tts
                 return;
             }
 
-            PromptBuilder sayAs = new PromptBuilder();
+            //PromptBuilder sayAs = new PromptBuilder();
+            MsTTSBuilder sayAs = new MsTTSBuilder();
             sayAs.StartVoice(selectedVoice);
             TimeSpan delay = new TimeSpan(0, 0, 1);
-            Add_As_DateTime_Text(ref sayAs, "2021-4-14 23:21");
+            sayAs.Add_As_DateTime_Text(/*ref sayAs, */"2021-4-14 23:21");
             sayAs.AppendBreak(delay);
-            Add_As_Date_Text(ref sayAs, "2021-4-1");
+            sayAs.Add_As_Date_Text(/*ref sayAs, */"2021-4-1");
             sayAs.AppendBreak(delay);
-            Add_As_Time_Text(ref sayAs, "23:15");
+            sayAs.Add_As_Time_Text(/*ref sayAs, */"23:15");
             sayAs.AppendBreak(delay);
-            Add_As_Fast_Text(ref sayAs, "这串文字很快啊");
+            sayAs.Add_As_Fast_Text(/*ref sayAs, */"这串文字很快啊");
             sayAs.AppendBreak(delay);
-            Add_As_Slow_Text(ref sayAs, "这串文字很慢啊");
+            sayAs.Add_As_Slow_Text(/*ref sayAs, */"这串文字很慢啊");
             sayAs.AppendBreak(delay);
-            Add_As_Normal_Text(ref sayAs, "这里加入了普通文字");
+            sayAs.Add_As_Normal_Text(/*ref sayAs, */"这里加入了普通文字");
             sayAs.AppendBreak(delay);
-            Add_As_Number_Text(ref sayAs, "12345");
+            sayAs.Add_As_Number_Text(/*ref sayAs, */"12345");
             sayAs.AppendBreak(delay);
-            Add_As_Number_Text(ref sayAs, "123.45");
+            sayAs.Add_As_Number_Text(/*ref sayAs, */"123.45");
             sayAs.AppendBreak(delay);
-            Add_As_SoundSoft_Text(ref sayAs, "这里声音很低");
+            sayAs.Add_As_SoundSoft_Text(/*ref sayAs, */"这里声音很低");
             sayAs.AppendBreak(delay);
-            Add_As_SoundMedium_Text(ref sayAs, "这里声音中等");
+            sayAs.Add_As_SoundMedium_Text(/*ref sayAs, */"这里声音中等");
             sayAs.AppendBreak(delay);
-            Add_As_SoundLoud_Text(ref sayAs, "这里声音很大");
+            sayAs.Add_As_SoundLoud_Text(/*ref sayAs, */"这里声音很大");
             sayAs.AppendBreak(delay);
-            Add_As_SpellOut_Text(ref sayAs, "这里 拼读 12345.123 clock is a fox");
+            sayAs.Add_As_SpellOut_Text(/*ref sayAs, */"这里 拼读 12345.123 clock is a fox");
             sayAs.AppendBreak(delay);
-            Add_As_Telephone_Text(ref sayAs, "(0759) 2134-567");
+            sayAs.Add_As_Telephone_Text(/*ref sayAs, */"(0759) 2134-567");
             sayAs.AppendBreak(delay);
-            Add_As_Time12_Text(ref sayAs, "3:45pm");
+            sayAs.Add_As_Time12_Text(/*ref sayAs, */"3:45pm");
             sayAs.AppendBreak(delay);
-            Add_As_Time12_Text(ref sayAs, "11:10am");
+            sayAs.Add_As_Time12_Text(/*ref sayAs, */"11:10am");
             sayAs.AppendBreak(delay);
-            Add_As_Time24_Text(ref sayAs, "13:20");
+            sayAs.Add_As_Time24_Text(/*ref sayAs, */"13:20");
             sayAs.AppendBreak(delay);
-            Add_As_Time24_Text(ref sayAs, "1:10");
+            sayAs.Add_As_Time24_Text(/*ref sayAs, */"1:10");
             sayAs.AppendBreak(delay);
-            Add_As_Time24_Text(ref sayAs, "12:00");
+            sayAs.Add_As_Time24_Text(/*ref sayAs, */"12:00");
             sayAs.AppendBreak(delay);
             sayAs.EndVoice();
 
